7f33b6cac70ef6f9738471c6c67fd871895d2d7c
Kobumbo <bartlomiej.janota@gmail.com>
2022-02-16 00:21:15 +0100

    init

--- 8/8.2/8.2.7.py
+++ 8/8.2/8.2.7.py
@@ -2,6 +2,7 @@ ciag = input('Podaj liczby oddzielone spacja: ')
 lista = ciag.split()
 maksimum = int(lista[0])
 maksimum2 =  int(lista[0])
+ile_razy = 0
 for i in range(len(lista)):
     liczba = int(lista[i])
     if maksimum < liczba:
@@ -9,7 +10,9 @@ for i in range(len(lista)):
         maksimum = liczba
     elif liczba > maksimum2 or maksimum2 == maksimum:
         maksimum2 = liczba
+for liczba in lista:
+    if int(liczba) == maksimum2:
+        ile_razy += 1
 print(maksimum2)
-
-
+print(ile_razy)
 
--- 8/8.2/8.2.8.py
+++ 8/8.2/8.2.8.py
@@ -2,11 +2,17 @@ ciag = input('Podaj liczby oddzielone spacja: ')
 lista = ciag.split()
 minimum = int(lista[0])
 minimum2 =  int(lista[0])
+ile_razy = 0
 for i in range(len(lista)):
     liczba = int(lista[i])
     if minimum > liczba:
         minimum2 = minimum
         minimum = liczba
-    elif liczba < minimum2  or minimum2 == minimum:
+    elif liczba < minimum2 or minimum2 == minimum:
         minimum2 = liczba
+for liczba in lista:
+    if int(liczba) == minimum2:
+        ile_razy += 1
 print(minimum2)
+print(ile_razy)
+
