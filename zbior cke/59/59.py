3055c57cc1f0899ce2066beb162406d7d42a4839
Kobumbo <bartlomiej.janota@gmail.com>
2022-03-09 15:13:17 +0100

    init

--- zbior cke/59/59.py
+++ zbior cke/59/59.py
@@ -1,3 +1,5 @@
+import math
+
 plik = open('liczby.txt', 'r')
 
 lista = plik.readlines()
@@ -6,31 +8,59 @@ for i in range(len(lista)):
     lista[i] = lista[i].strip()
 
 
+
+def iloczyn_cyfr(liczba):
+    iloczyn = 1
+    liczba = str(liczba)
+    for i in liczba:
+        iloczyn *= int(i)
+    return iloczyn
+
+def moc_liczby(liczba):
+    moc = 1
+    liczba = iloczyn_cyfr(liczba)
+    while liczba > 9:
+        liczba = iloczyn_cyfr(liczba)
+        moc += 1
+    return moc
+
+# def rozklad2(liczba):
+#     czynniki = []
+#     x = 2
+#     if liczba % 2 == 0:
+#         return 0
+#     while liczba >= 2:
+#         while liczba % x == 0:
+#             liczba //= x
+#             if x not in czynniki:
+#                 czynniki.append(x)
+#             if len(czynniki) > 3:
+#                 return 0
+#         x += 1
+#     return len(czynniki)
+
 def rozklad(liczba):
     czynniki = []
-    x = 2
     if liczba % 2 == 0:
         return 0
-    while liczba >= 2:
-        while liczba % x == 0:
-            liczba //= x
-            if x not in czynniki:
-                czynniki.append(x)
+    for i in range(3, int(math.sqrt(liczba)) + 1, 2):
+        while liczba % i == 0:
+            liczba = liczba / i
+            if i not in czynniki:
+                czynniki.append(i)
             if len(czynniki) > 3:
                 return 0
-        x += 1
+    if liczba > 2:
+        czynniki.append(liczba)
     return len(czynniki)
-
 
 def a():
     ile = 0
     for i in lista:
         if rozklad(int(i)) == 3:
             ile += 1
-            print(ile)
-    print('Koniec')
     print(ile)
-    print('Koniec')
+
 
 
 def b():
@@ -40,6 +70,22 @@ def b():
             ile += 1
     print(ile)
 
+def c():
+    lista_mocy = []
+    lista_mocy_1 = []
+    for i in lista:
+        lista_mocy.append(moc_liczby(i))
+    for i in range(1,max(lista_mocy)+1):
+        print(f'moc {i}: {lista_mocy.count(i)}')
+    for i in lista:
+        if moc_liczby(i) == 1:
+            lista_mocy_1.append(int(i))
+    print()
+    print(f'maksymalna liczba o mocy 1: {max(lista_mocy_1)}')
+    print(f'minimalna liczba o mocy 1: {min(lista_mocy_1)}')
+            
 
 # a()
-b()
+# b()
+# c()
+
